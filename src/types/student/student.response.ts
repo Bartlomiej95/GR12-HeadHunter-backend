c2bd8ab505fce0c684806230a4fd20792fc6a9fc
enrju <enrju@enrju.pl>
2022-07-28 16:09:36 +0200

    add interface StudentCVResponse

--- src/types/student/student.response.ts
+++ src/types/student/student.response.ts
@@ -1,4 +1,34 @@
+import { rating } from "../user/user.import.type";
+import {ExpectedContractType, ExpectedTypeWork } from "../user/user.register.type";
+
 export interface ChangeStudentStatusResponse {
     actionStatus: boolean;
     data: string | null;
+}
+
+export interface StudentCVResponse {
+    // id: string;      //???
+    email: string;
+    courseCompletion: rating;
+    courseEngagment: rating;
+    projectDegree: rating;
+    teamProjectDegree: rating;
+    bonusProjectUrls: string[];
+    tel: number;
+    firstName: string;
+    lastName: string;
+    githubUsername: string;
+    portfolioUrls: string[];
+    projectUrls: string[];
+    bio: string;
+    expectedTypeWork: ExpectedTypeWork;
+    targetWorkCity: string;
+    expectedContractType: ExpectedContractType;
+    expectedSalary: number | null;
+    canTakeApprenticeship: boolean;
+    monthsOfCommercialExp: number;
+    education: string | null;
+    workExperience: string;
+    courses: string;
+    hrId: string | null;
 }
